700354bfe7d2a315a319b91b836bba9ce04be17e
Naresh <naresh_rangu@epam.com>
2019-08-05 16:18:38 +0530

    Commit Helloname

---
+++ src/main/java/com/ranguht/code/javabasics/Helloname.java
@@ -0,0 +1,11 @@
+package com.ranguht.code.javabasics;
+
+public class Helloname
+{
+    public static void main(String[] args) {
+        for(int i=0;i<args.length;i++)
+        {
+            System.out.println("Hello" +" "+args[i]);
+        } }
+}
+
