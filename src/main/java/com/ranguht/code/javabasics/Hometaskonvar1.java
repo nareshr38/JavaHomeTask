f3bf1091c8068da1aa44d4aae4eaa2d08872e779
Naresh <naresh_rangu@epam.com>
2019-08-05 16:20:50 +0530

    Commit Hometaskonvar1

---
+++ src/main/java/com/ranguht/code/javabasics/Hometaskonvar1.java
@@ -0,0 +1,14 @@
+package com.ranguht.code.javabasics;
+
+public class Hometaskonvar1 {
+        public static void main(String[] args) {
+            double total = countOfApples1(3.5, 2.5);
+
+            System.out.println("total number of apples" + " " + total);
+        }
+
+        public static double countOfApples1(double Daniel, double Amber) {
+            double Countofapples1 = Daniel + Amber;
+            return Countofapples1;
+        }
+    }
