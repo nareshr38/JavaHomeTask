a06fee9d0a5f3628c9cbb20007d0bd236c07ed2e
Naresh <naresh_rangu@epam.com>
2019-08-05 16:13:35 +0530

    Commit CanBalance

---
+++ src/main/java/com/ranguht/code/javabasics/CanBalance.java
@@ -0,0 +1,28 @@
+package com.ranguht.code.javabasics;
+
+public class CanBalance {
+    public static void main(String[] args) {
+        int [] CanBalancedata=  {1, 1, 1, 2, 1};
+        boolean canBalanceResult = canBalance(CanBalancedata);
+        System.out.println(canBalanceResult);
+
+    }
+
+    public static boolean canBalance(int[] nums) {
+        int first = 0;
+        int second = 0;
+
+        for(int i = 0; i < nums.length; i++)
+            second += nums[i];
+
+        for(int i = 0; i <= nums.length - 2; i++) {
+            first += nums[i];
+            second -= nums[i];
+
+            if(first == second)
+                return true;
+        }
+
+        return false;
+    }
+}
