1dc4ea379c95a51012baaebc338722fade4611d7
Naresh <naresh_rangu@epam.com>
2019-08-05 17:06:12 +0530

    Commit WithoutString

---
+++ src/main/java/com/ranguht/code/javabasics/WithoutString.java
@@ -0,0 +1,26 @@
+package com.ranguht.code.javabasics;
+
+public class WithoutString {
+    public static void main(String[] args) {
+        String WithoutStringResult=withoutString("Hello there", "llo");
+        System.out.println(WithoutStringResult);
+    }
+    public static String withoutString(String base, String remove) {
+        String result = "";
+        for (int i = 0; i < base.length(); i++) {
+            if (i > base.length() - remove.length()) {
+                result += base.substring(i, base.length());
+                break;
+            } else {
+                String substring = base.substring(i, i + remove.length());
+                if (!(substring.equalsIgnoreCase(remove))) {
+                    result += base.charAt(i);
+                } else {
+                    i = i + remove.length() - 1;
+                }
+            }
+        }
+        return result;
+    }
+
+}
