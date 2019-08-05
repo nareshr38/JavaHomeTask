243d5ec44749dbb6a4224539f7429e8fffd4453a
Naresh <naresh_rangu@epam.com>
2019-08-05 16:11:26 +0530

    Commit Calculate

---
+++ src/main/java/com/ranguht/code/javabasics/Calculate.java
@@ -0,0 +1,25 @@
+package com.ranguht.code.javabasics;
+
+public class Calculate {
+    public static void main(String[] args) {
+
+        double firstNumber = Double.parseDouble(args[0]);
+        double secondNumber = Double.parseDouble(args[1]);
+        String operation = args[2];
+
+        if (operation.equals("+")) {
+            System.out.println("Addition : " + (firstNumber + secondNumber));
+        } else if (operation.equals("-")) {
+            System.out.println("Subtraction :" + (firstNumber - secondNumber));
+        } else if (operation.equals("*")) {
+            System.out.println("Multiplication :" + firstNumber * secondNumber);
+        } else if (operation.equals("/")) {
+            System.out.println("Division :" + (firstNumber / secondNumber));
+        }
+        else if (operation.equals("%")) {
+            System.out.println("Remainder :" + (firstNumber % secondNumber));
+        }  else {
+            System.out.println("percentage:"+(firstNumber / secondNumber)*100);
+        }
+    }
+}
