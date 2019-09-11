8c85a69c2b2e529351fbea8547c49d0c7b33100f
Naresh <naresh_rangu@epam.com>
2019-09-11 12:02:03 +0530

    commit collection pgms hashset

--- src/main/java/com/ranguht/code/javabasics/CollectionLinkedList11Sep2019.java
+++ src/main/java/com/ranguht/code/javabasics/CollectionLinkedList11Sep2019.java
@@ -4,11 +4,9 @@ import java.util.LinkedList;
 public class CollectionLinkedList11Sep2019 {
     public static void main(String[] args) {
         LinkedList<String> ll=new LinkedList<String>();
-
         ll.add("Rangu");
         ll.add("Naresh");
         ll.add("QA");
-
         Iterator<String> itr=ll.iterator();
                 while (itr.hasNext())
                     System.out.println(itr.next());
@@ -20,6 +18,22 @@ public class CollectionLinkedList11Sep2019 {
         Iterator<String>itr1=des1.descendingIterator();
         while (itr1.hasNext())
             System.out.println(itr1.next());
+
+      /*  LinkedList<String> met=new LinkedList<String>();
+        met.add("quick");
+        met.add("set");
+        Iterator<String>txt =met.getFirst();
+        while(txt.hasNext())
+            System.out.println(txt.next());
+*/
+        LinkedList<String> sr=new LinkedList<String>();
+        sr.add("sr");
+        sr.add("tx");
+        sr.getFirst();
+        Iterator<String> tsr=sr.iterator();
+        while (tsr.hasNext())
+            System.out.println(tsr.next());
+
 
 
     }
---
+++ src/main/java/com/ranguht/code/javabasics/CollectionsHashSet11Sep2019.java
@@ -0,0 +1,22 @@
+package com.ranguht.code.javabasics;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class CollectionsHashSet11Sep2019 {
+    public static void main(String[] args) {
+        ArrayList<String> list= new ArrayList<String>();
+        list.add("Nar");
+        list.add("Ran");
+        list.add("Nar"); //dup
+        list.add("QA");
+
+        HashSet<String>hs=new HashSet<String>(list);
+        Iterator<String>itr=hs.iterator();
+        while (itr.hasNext())
+            System.out.println(itr.next());
+
+
+    }
+}
