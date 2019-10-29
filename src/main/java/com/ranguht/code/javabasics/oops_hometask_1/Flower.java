927bb7acce87ba194c29bb5daad609442d8da750
Naresh <naresh_rangu@epam.com>
2019-10-29 18:36:32 +0530

    Commit oop task1

--- src/main/java/com/ranguht/code/javabasics/oops_hometask_1/Flower.java
+++
@@ -1,7 +0,0 @@
-package com.ranguht.code.javabasics.oops_hometask_1;
-
-public interface Flower {
-
-    public int costOfFlowers(String typeOfRose, int cost, int quantity);
-
-}
--- src/main/java/com/ranguht/code/javabasics/oops_hometask_1/LillyFlower.java
+++
@@ -1,8 +0,0 @@
-package com.ranguht.code.javabasics.oops_hometask_1;
-
-public class LillyFlower extends RoseFlower {
-    @Override
-    public int costOfFlowers(String typeOfRose, int cost, int quantity) {
-        return super.costOfFlowers(typeOfRose, cost, quantity);
-    }
-}
